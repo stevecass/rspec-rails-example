00293d5ad6158e0850002c29b5a0f0d6130aa890
Steven Cassidy <steven@mumsnet.com>
2015-07-28 17:32:17 -0400

    lose seed data

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-require 'factory_girl_rails'
-require 'faker'
-User.delete_all
-Post.delete_all
-FactoryGirl.create :user, :email => "admin@example.com", password:'123456'
-10.times do
-  Post.create!(title:Faker::Commerce.product_name, content: Faker::Lorem.paragraph)
-
-end
